a55e2f3388d0bc24a47cf4446a5d09f25252dc09
Chelsea <cnwankpa@live.com>
2017-11-28 16:26:31 -0600

    Adding repo files with folder structure, survey and html pages, and server file

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+var port = process.env.PORT || 8080;
+
+//json parser
+var jsonParser = bodyParser.json();
+
+//url encoder app
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+
+//custom JSON as json
+app.use(bodyParser.json({type: 'application/*+json' }));
+
+//buffer
+app.use(bodyParser.raw({ type: 'application/vmd.custom-type' }));
+
+//make HTML body a string
+app.use(bodyParser.text({ type: 'text/html'}));
+
+app.listen(port, function(){
+	console.log("App Listening on port: " + port);
+});
