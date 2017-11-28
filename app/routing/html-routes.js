dac3bb1a908ba5ca87309943d2d5f6e8d6ae56cb
Chelsea <cnwankpa@live.com>
2017-11-28 16:39:18 -0600

    edited error

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -2,13 +2,13 @@ var path = require('path');
 
 module.exports = function(app){
 
-	app.get("/survey", function(req,res) ){
+	app.get("/survey", function(req,res) {
 		res.sendFile(path.join(_dirname + "/../public/survey.html"));
 	});
 
 //Not using pre-defined homepage so that if user is on the application / sit e 
 //and not on a specific page they are directed to home
-	app.use(function(req,res) ){
+	app.use(function(req,res) {
 		res.sendFile(path.join(_dirname + "/../public/home.html"));
 	});
 
