20360bc9d9cb7b168379385b3eb75ca019e610cc
Chelsea <cnwankpa@live.com>
2017-11-28 16:40:23 -0600

    edited dirname error

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -3,13 +3,13 @@ var path = require('path');
 module.exports = function(app){
 
 	app.get("/survey", function(req,res) {
-		res.sendFile(path.join(_dirname + "/../public/survey.html"));
+		res.sendFile(path.join(__dirname + "/../public/survey.html"));
 	});
 
 //Not using pre-defined homepage so that if user is on the application / sit e 
 //and not on a specific page they are directed to home
 	app.use(function(req,res) {
-		res.sendFile(path.join(_dirname + "/../public/home.html"));
+		res.sendFile(path.join(__dirname + "/../public/home.html"));
 	});
 
 }
